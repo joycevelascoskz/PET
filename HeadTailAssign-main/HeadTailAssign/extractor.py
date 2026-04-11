0ebca8097d4b837beb62ee6d5e4ed94981d12c10
joycevelascoskz <joycevelasco.skz@gmail.com>
2026-04-11 10:05:59 +0800

    Add files via upload

---
+++ HeadTailAssign-main/HeadTailAssign/extractor.py
@@ -0,0 +1,135 @@
+from collections import defaultdict
+import glob, os
+import numpy as np
+import re
+import pandas as pd
+import csv
+import shutil
+
+from HeadTailAssign.extractor_helper import Helper
+
+
+class Extractor:
+    '''Class related to data extraction.
+
+    Methods:
+        op_extractor(self): Extracts only the data regarding Mulliken Atomic Overlap Populations from .out file. Saves .txt file. Returns None.
+        rx_extractor(self): Extracts the Rx value organized by higher to lower value. Returns a dataframe.
+        get_head_tail(self): Gets head and tail data from a .csv file and creates a dataframe. Returns a dataframe.
+        analyze_head_tail(self): Analyzes the head and tail data to compare if the information is equal (add True) or different (add False). Returns a dataframe.
+        '''
+    def op_extractor(self, name_dir: str): 
+        '''Extracts only the data regarding Mulliken Atomic Overlap Populations from .out file. Saves .txt file. Returns None.
+         
+        Arguments:
+            name_dir(str) = Name of the output directory'''
+        try:
+            for root, dirs, files in os.walk(name_dir):
+                for file in files:
+                    if file.endswith(".log"):
+                        fileName = re.sub(".log", "", file)
+                        path = root + "\\" + file # use / to run in linux
+                        path_output = root + "\\" + fileName + ".txt" # use / to run in linux
+
+                        #https://stackoverflow.com/questions/61172255/how-to-slice-data-from-a-text-file-given-the-desired-range-of-lines
+                        with open(path, encoding="latin-1") as f:
+                            with open(path_output, "w") as out:
+                                print(f'Analyzing {file}...')
+                                out.write(f'{file}\n')
+                                should_write=False
+                                for x, line in enumerate(f):
+                                    if line.find('               ----- POPULATIONS IN EACH AO -----')!=-1:
+                                        should_write=False
+                                    if should_write:
+                                        out.write(line)
+                                    if line.find('     ATOMIC MULLIKEN POPULATION IN EACH MOLECULAR ORBITAL')!=-1:
+                                        out.write('     ATOMIC MULLIKEN POPULATION IN EACH MOLECULAR ORBITAL\n')
+                                        should_write=True
+                                    
+                                    if line.find(' SYMMETRIES FOR INITIAL GUESS ORBITALS FOLLOW.   BOTH SET(S).')!=-1:
+                                        out.write('\n     OCCUPIED ORBITALS\n')
+                                        for i in range(1):
+                                            out.write(next(f))
+                                            out.write(f'\n---------------------')
+                                    
+                                    if line.find('          INTERNUCLEAR DISTANCES (ANGS.)')!=-1:
+                                        should_write=False
+                                    if line.find('           CHARGE         X                   Y                   Z')!=-1:
+                                        out.write('\n     ATOMS\n')
+                                        should_write=True
+
+                                        
+                            if os.stat(path_output).st_size >= 100:
+                                print(f'file {fileName}.txt was generated sucessfully.')
+                            else:
+                                f.close()
+                                print(f'WARNING: {fileName} could not be generated.')
+                                os.remove(path_output)
+                                                
+        except ValueError:
+            raise ValueError("There is no files with .log extension.")
+
+        return
+    
+    def rx_extractor(self, name_dir):
+        '''Extracts the Rx value organized by higher to lower value. Returns a dataframe.
+        
+        Arguments:
+            name_dir(str) = Name of the output directory.'''
+        #global results
+        try:
+            for root, dirs, files in os.walk(name_dir):
+                for file in files:
+                    if file.endswith(".txt"):
+                        fileName = re.sub(".txt", "", file)
+                        path = root + "\\" + file # use / to run in linux
+                        path_output = root + "\\" + fileName # use / to run in linux
+
+                        if os.stat(path).st_size >= 100:
+                            print(f'Analyzing {file}...')
+                            helper = Helper()
+                            n_orbitals = helper._get_n_orbitals(path)
+                            atoms = helper._get_atoms_list(path)
+                            n_id = helper._get_id(path)
+                            a_population = helper._get_a_population(path, n_orbitals, n_id)
+
+                            results = helper._get_dataframe(atoms, n_id, a_population, path_output)
+
+        except ValueError:
+            raise ValueError("There is no files with .txt extension.")
+
+        return results
+
+    def get_head_tail(self, name_dir: str):
+        '''Gets head and tail data from a .csv file and creates a dataframe. Returns a dataframe.
+
+        Arguments:
+            name_dir(str) = Name of the output directory.'''
+        os.chdir(name_dir)
+        d = defaultdict(list)
+        for file in glob.glob("*.csv"):
+            if file == 'input.csv':
+                pass
+            else:
+                fileName = re.sub(".csv", "", file)
+                df = pd.read_csv(file)
+                d['name'].append(fileName)
+                d['head output'].append(df.iloc[1,1])
+                d['tail output'].append(df.iloc[0,1])
+        df1 = pd.DataFrame.from_dict(d)
+
+        helper = Helper()
+        helper._merge_head_tail(df1)
+
+        os.chdir("../")
+        return df1
+
+    def analyze_head_tail(self):
+        '''Analyzes the head and tail data to compare if the information is equal (add True) or different (add False). Returns a dataframe.'''
+        df = pd.read_csv('head_tail_comparison.csv', sep=',')
+        #https://stackoverflow.com/questions/27474921/compare-two-columns-using-pandas
+        df['comparison'] = np.where((df['head input'] == df['head output']) & (df['tail input'] == df['tail output'])
+                     , 'True', 'False')
+        df.to_csv(f"smi2gamess-output-comparison.csv", sep=",", index=False, quoting=csv.QUOTE_ALL)
+
+        return df
---
+++ HeadTailAssign-main/HeadTailAssign/extractor_helper.py
@@ -0,0 +1,171 @@
+import pandas as pd
+import csv
+import re
+import math
+
+
+class Helper:
+    '''Class related to helper functions.
+
+    Methods:
+        _get_atoms_list(self, path): Non-public method. Gets the atom type on the file .txt. Returns a list.
+        _get_n_orbitals(self, path): Non-public method. Gets the number of orbitals calculated by GAMESS. Returns a list.
+        _get_id(self, path): Non-public method. Gets the atom id. Returns a list.
+        _get_a_population(self, path, n_orbitals): Non-public method. Gets the atomic population calculated by GAMESS. Returns a list.
+        _change_atom_ID(self, atomId): Changes the identification number of the atoms by minus one. Returns a list
+        _get_dataframe(self, valence_orbital_list, filename): Gets a dataframe from lists with informations about the atoms. Returns a dataframe.
+        _merge_head_tail(self, df): Merges the input dataframe with the output dataframe. Returns a dataframe.
+    '''
+
+    def _get_atoms_list(self, path):
+        '''Non-public method. Gets the atom type on the file .txt. Returns a list.
+        
+        Arguments:
+            path: path file with the information regarding atom types.
+            '''
+        with open(path, encoding='utf8') as f:
+            should_append = False
+            atoms = []
+            for x, line in enumerate(f):
+                if line.find('          INTERNUCLEAR DISTANCES (ANGS.)')!=-1:
+                    should_append=False
+                if should_append:
+                    if line[1:3]:
+                        atoms.append(line[1:3])
+                if line.find('     ATOMS')!=-1:
+                    should_append=True
+            atoms_clean = [a.strip(' ') for a in atoms]
+        return atoms_clean
+    
+    def _get_n_orbitals(self, path):
+        '''Non-public method. Gets the number of orbitals calculated by GAMESS. Returns a list.
+        
+        Arguments:
+            path: path file with the information regarding orbitals.
+            '''
+        with open(path, encoding='utf8') as f:
+            n_orbitals = []
+            for x, line in enumerate(f):
+                if re.search('ORBITALS ARE OCCUPIED', line):
+                    n_orbitals.append(line[4:7])
+            n_orbitals_clean = [o.strip(' ') for o in n_orbitals]
+            
+        return n_orbitals_clean
+    
+    def _get_id(self, path):
+        '''Non-public method. Gets the atom id. Returns a list.
+        
+        Arguments:
+            path: path file with the information of atomic id.
+            '''
+        with open(path, encoding='utf8') as f:
+            n_id = []
+            should_append = False
+            for x, line in enumerate(f):
+                if should_append:
+                    if line:
+                        n_id.append(line[0:5])
+                    else:
+                        break
+                if line.find('---------------------     ATOMIC MULLIKEN POPULATION IN EACH MOLECULAR ORBITAL')!=-1:
+                    should_append=True
+            n_id_clean = [i.strip(' ') for i in n_id]
+            n_id_clean_a = []
+            for n in n_id_clean:
+                if n == '\n':
+                    continue
+                if n == '':
+                    continue
+                else:
+                    if n not in n_id_clean_a:
+                        n_id_clean_a.append(n)
+
+        return n_id_clean_a
+    
+    def _get_a_population(self, path, n_orbitals, n_id):
+        '''Non-public method. Gets the atomic population calculated by GAMESS. Returns a list.
+        
+        Arguments:
+            path: path file with the information atomic population. UPDATE HERE
+            '''
+
+        with open(path, encoding='utf8') as f:
+            o_p = []
+            should_append = False
+            count = 0
+            div_rest = (int(n_orbitals[0]) % 5)
+            div = math.ceil(int(n_orbitals[0])/5)
+            if div_rest == 0:
+                div_rest = 5
+            n_id_compare = []
+            for x, line in enumerate(f):
+                
+                if should_append:
+                    for n in n_id:
+                        o_id = line[0:6]
+                        c_line = o_id.replace(" ", "")
+                        if n == c_line:
+                            n_id_compare.append(c_line)
+                        elif n_id_compare == n_id:
+                            count += 1
+                            n_id_compare = []
+                        if count == div-1:
+                            if n == c_line:
+                                if div_rest == 1:
+                                    o_p.append(line[18:27].replace("\n", ""))
+                                if div_rest == 2:
+                                    o_p.append(line[29:38].replace("\n", ""))
+                                if div_rest == 3:
+                                    o_p.append(line[40:49].replace("\n", ""))
+                                if div_rest == 4:
+                                    o_p.append(line[51:60].replace("\n", ""))
+                                if div_rest == 5:
+                                    o_p.append(line[62:71].replace("\n", ""))
+                if line.find('---------------------     ATOMIC MULLIKEN POPULATION IN EACH MOLECULAR ORBITAL')!=-1:
+                    should_append=True
+        return o_p
+
+    def _change_atom_ID(self, atomId):
+        '''Non-public method. Changes the identification number of the atoms by minus one. Returns a list.
+        
+        Arguments:
+            atomId (list): Identification number of the atoms.
+            ''' 
+        atomIdList = []
+        for i in atomId:
+            newID = int(i)
+            atomIdList.append(newID)
+        
+        return atomIdList
+
+    def _get_dataframe(self, atoms, n_id, a_population, filename):
+        '''Non-public method. Gets a dataframe from lists with informations about the atoms. Returns a dataframe.
+        
+        Arguments:
+            valence_orbital_list (list): List with only valence orbitals.
+            filename (str): Name of the files without the extension.
+            ''' 
+
+        # atomIdList = self._change_atom_ID(atomId)
+       
+        df = pd.DataFrame(
+            {'atom': atoms,
+            'id': n_id,
+            'Mulliken population': a_population}
+        )
+        df.sort_values(by=['Mulliken population'], ascending=False, inplace=True)
+        df.to_csv(f"{filename}.csv", sep=",", index=False, quoting=csv.QUOTE_ALL)
+        
+        return df
+
+    def _merge_head_tail(self, df):
+        '''Non-public method. Merges the input dataframe with the output dataframe. Returns a dataframe.
+        
+        Arguments:
+            df (dataframe): Input dataframe. 
+            ''' 
+        df1 = pd.read_csv('input.csv', sep=',', names=["name","reaction","monomers","classes","mechanism"])
+        df2 = df1.merge(df, on='name')
+        df2.to_csv(f"head_tail_comparison.csv", sep=",", index=False, quoting=csv.QUOTE_ALL)
+
+        return df2
